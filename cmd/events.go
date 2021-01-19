99d463f88e507ae9c913c4aa83fd404084f246d9
Caio Almeida <cai0_almeida@outlook.com>
2021-01-18 23:21:44 -0300

    add flood command

--- cmd/events.go
+++ cmd/events.go
@@ -1,5 +1,6 @@
 package main
 
 import (
+	_ "github.com/muriboistas/zapzap/commands/flood"
 	_ "github.com/muriboistas/zapzap/commands/ping"
 )
---
+++ commands/flood/flood.go
@@ -0,0 +1,36 @@
+package flood
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/muriboistas/zapzap/commands"
+	"github.com/muriboistas/zapzap/infra/whats/message"
+
+	"github.com/Rhymen/go-whatsapp"
+)
+
+func init() {
+	commands.New("flood", flood).SetHelp("flood some message").SetCooldown(2).Add()
+}
+
+func flood(wac *whatsapp.Conn, msg whatsapp.TextMessage) {
+	fmt.Println(msg.Info.Source.String())
+	rand.Seed(time.Now().UnixNano())
+	times := rand.Intn(5)
+	ticker := time.NewTicker(1 * time.Second)
+	quit := make(chan struct{})
+	go func() {
+		for i := 0; i <= times+7; i++ {
+			select {
+			case <-ticker.C:
+				message.Reply(msg.Text, wac, msg)
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+		ticker.Stop()
+	}()
+}
